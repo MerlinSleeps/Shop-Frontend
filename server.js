43f89099d417b15b2362114ed4ca99d6884d4d9c
Merlin <merlin199919@gmail.com>
2020-04-24 03:25:30 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/dist/WarehouseFrontend`));
+
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/WarehouseFrontend/index.html`);
+});
+
+// Start the app
+app.listen(process.env.PORT || 9090);
